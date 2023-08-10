fad04c43104fb469830db96667323cf21faaca59
Arshalan <ansariarsh048@gmail.com>
2023-08-10 21:46:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// let i=5;
+// console.log(i);
+
+// let a="hii there";
+// console.log(a);
+
+// let pwCoursePrice=500;
+// let gst=36;
+// let finalAmmount= pwCoursePrice+gst;
+// console.log(finalAmmount);
+
+// let dsaPrice=300;
+// console.log("total Price:",dsaPrice+gst);
+
+// console.log("answer is:", 5%2);
+// console.log("answer is:", 5/2);
+// console.log("answer is:", 5**2);
+
+// comprition
+
+// let ayushScore=200;
+// let abhayScore="200";
+// // console.log(ayushScore==abhayScore);
+// // console.log(ayushScore>=abhayScore);
+// // console.log(ayushScore<=abhayScore);
+// console.log(ayushScore===abhayScore);
+
+// let value1= true;
+// let value2= false;
+// let value3= false;
+// let value4= true;
+
+// console.log(value1&&value2);
+// console.log(value1&&value4);
+// console.log(value1||value4);
+// console.log(value1||value2);
+
+let loggedin=true
+let gmailAccount=true;
+let emailAccount=false;
+console.log(gmailAccount||emailAccount);
